6c9d013917f30a18bc662c1c4425a9c689d1daa7
Lirous587 <2115883273@qq.com>
2025-05-06 10:38:53 +0800

    项目初始配置

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
+import DefaultLayout from './layout/DefaultLayout.vue'
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+
+const layouts = {
+  DefaultLayout: defineAsyncComponent(() => import('./layout/DefaultLayout.vue')),
+  AuthLayout: defineAsyncComponent(() => import('./layout/AuthLayout.vue')),
+}
+
+const route = useRoute()
+
+const currentLayout = computed(() => {
+  const layoutName = (route.meta.layout as keyof typeof layouts) || 'DefaultLayout'
+  return layouts[layoutName]
+})
+</script>
+
+<template>
+  <component :is="currentLayout"> </component>
+</template>
